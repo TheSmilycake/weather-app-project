ccf89f09821cc432c671de443883bc43ebe954de
TheSmilycake <wiedaurebecca@gmail.com>
2022-06-14 23:37:57 +0200

    nicu desu improvu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Search from './Search'
 import Result  from './Result/Result'
+
+
+
 function App() {
     const [weatherData, setWeatherData] = useState("Miau");
     function updateWeatherData(apiResponse) {
@@ -21,16 +24,3 @@ function App() {
 }
 
 export default App;
-// const [weatherData, setWeatherData] = useState({});
-//
-//     function logWeatherData(searchData) {
-//        setWeatherData(searchData);
-//        console.log(weatherData);
-//     }
-//
-//     return (
-//         <div className="App">
-//             <h1>Returning State Back to Parent</h1>
-//             <Search getData={logWeatherData} />
-//         </div>
-//     );
